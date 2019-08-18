c61c345a9865d1909e41e59da98f8b3daa5b9aa8
gps2601 <smith260194@gmail.com>
2019-08-18 22:43:09 +0100

    restructing project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+const mongoUrl = 'mongodb+srv://gps2601:test123@fieldnote-3fejg.mongodb.net/bloglist?retryWrites=true&w=majority'
+console.log('connecting to  ', mongoUrl)
+
+mongoose.connect(mongoUrl, {useNewUrlParser: true})
+    .then(() => {
+        console.log('connected to mongodb')
+    })
+    .catch((error) => {
+        console.log('error connecting to mongodb', error.message)
+    })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(blogsRouter)
+
+module.exports = app
