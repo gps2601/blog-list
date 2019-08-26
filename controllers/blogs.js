bbece0a6efac5f253cff45fb46f95598af4d2c11
gps2601 <smith260194@gmail.com>
2019-08-26 20:58:32 +0100

    Adding an update endpoint

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -19,4 +19,28 @@ blogsRouter.post('/api/blogs', async (request, response) => {
     }
 })
 
+blogsRouter.delete('/api/blogs/:id', async (request, response, next) => {
+    try {
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch(exception) {
+        next(exception)
+    }
+})
+
+blogsRouter.put('/api/blogs/:id', async (request, response, next) => {
+
+    const body = request.body
+    const blog = {
+        ...body
+    }
+
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+        response.status(204).json(updatedBlog)
+    } catch (exception) {
+        next(exception)
+    }
+})
+
 module.exports = blogsRouter
